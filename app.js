07f7f7d8dca642877485740dd06e5a51da9eced1
Alvaro Vinicus <d4rkgamepaly@gmail.com>
2025-03-07 17:01:26 -0300

    Adicionada a função de sortear e comentarios pra clarificar o código */

--- app.js
+++ app.js
@@ -11,17 +11,29 @@ if (nome.trim() !== ''){ //verifica se o campo está vazio e depois remove os es
     alert('Por favor, insira um nome'); //alerta caso o campo esteja vazio
 }
     document.getElementById('amigo').value = ''; //limpa o campo após a inserção
-    console.log(amigos)  //adicionado pra debugar, retirar depois de verificar o funcionamento
 }
 
 function atualizarListaAmigos() {
     const listaAmigos = document.getElementById('listaAmigos');
     listaAmigos.innerHTML = ''; // Limpa a lista existente
   
-    for (let nome of amigos) {
+    for (let nome of amigos) { //cria um item de lista para cada nome
       const itemLista = document.createElement('li');
-      itemLista.textContent = nome;
-      itemLista.setAttribute('role', 'listitem');
-      listaAmigos.appendChild(itemLista);
+      itemLista.textContent = nome; //adiciona o nome ao item de lista
+      itemLista.setAttribute('role', 'listitem'); //adiciona um atributo ao item de lista
+      listaAmigos.appendChild(itemLista); //adiciona o item de lista à lista de amigos
     }
-  }
+}
+
+//Função que sorteia um amigo
+function sortearAmigo() {
+    if (amigos.length === 0) { //Verifica se a lista de amigos está vazia
+      alert('Adicione amigos antes de sortear!');
+      return;
+    }
+  
+    const indiceSorteado = Math.floor(Math.random() * amigos.length); //sorteia um indice da lista de amigos
+    const amigoSorteado = amigos[indiceSorteado];
+  
+    document.getElementById('resultado').innerHTML = `<p>O amigo sorteado foi: ${amigoSorteado}</p>`; //exibe o amigo sorteado
+}

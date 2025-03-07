d9af3e0e8d9116e393d4dc69146206521d6487f4
Alvaro Vinicus <d4rkgamepaly@gmail.com>
2025-03-07 00:54:19 -0300

    Adicionada a função que insere nomes no array de nomes.

--- app.js
+++ app.js
@@ -1,2 +1,12 @@
 //Array para armazenar os nomes
 let amigos = [];
+
+//Função que adiciona nomes ao array
+function addAmigo(){
+    let nome = document.getElementById('nome').value
+    if (nome() == ''){
+        amigos.push(nome);
+}
+    document.getElementById('nome').value = '';
+    console.log(amigos)  //adicionado pra debugar, retirar depois de verificar o funcionamento
+}

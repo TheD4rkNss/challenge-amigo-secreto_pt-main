2cb11c4126b0289c700bab6d12041094e7cf9ca6
Alvaro Vinicus <d4rkgamepaly@gmail.com>
2025-03-07 01:08:08 -0300

    Adicionado comentarios e melhorias na função AddAmigo */

--- app.js
+++ app.js
@@ -3,10 +3,12 @@ let amigos = [];
 
 //Função que adiciona nomes ao array
 function addAmigo(){
-    let nome = document.getElementById('nome').value
-    if (nome() == ''){
+    let nome = document.getElementById('nome').value //pega o valor do campo de texto
+    if (nome.trim() !== ''){ //verifica se o campo está vazio e depois remove os esopaços em branco
         amigos.push(nome);
+} else {
+    alert('Por favor, insira um nome'); //alerta caso o campo esteja vazio
 }
-    document.getElementById('nome').value = '';
+    document.getElementById('nome').value = ''; //limpa o campo após a inserção
     console.log(amigos)  //adicionado pra debugar, retirar depois de verificar o funcionamento
 }

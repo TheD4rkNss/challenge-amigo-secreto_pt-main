8e56c37ed93836b06f93109fbcb2c45e052e0223
Alvaro Vinicus <d4rkgamepaly@gmail.com>
2025-03-07 16:39:21 -0300

    Adicionada a a funcionalidade de atualizar a lista de amigos e mudanças feita nos nomes das variáveis e funções para combinar com o HTML

--- app.js
+++ app.js
@@ -2,13 +2,26 @@
 let amigos = [];
 
 //Função que adiciona nomes ao array
-function addAmigo(){
-    let nome = document.getElementById('nome').value //pega o valor do campo de texto
-    if (nome.trim() !== ''){ //verifica se o campo está vazio e depois remove os esopaços em branco
-        amigos.push(nome);
+function adicionarAmigo(){
+    let nome = document.getElementById('amigo').value //pega o valor do campo de texto
+if (nome.trim() !== ''){ //verifica se o campo está vazio e depois remove os esopaços em branco
+    amigos.push(nome);
+    atualizarListaAmigos();
 } else {
     alert('Por favor, insira um nome'); //alerta caso o campo esteja vazio
 }
-    document.getElementById('nome').value = ''; //limpa o campo após a inserção
+    document.getElementById('amigo').value = ''; //limpa o campo após a inserção
     console.log(amigos)  //adicionado pra debugar, retirar depois de verificar o funcionamento
 }
+
+function atualizarListaAmigos() {
+    const listaAmigos = document.getElementById('listaAmigos');
+    listaAmigos.innerHTML = ''; // Limpa a lista existente
+  
+    for (let nome of amigos) {
+      const itemLista = document.createElement('li');
+      itemLista.textContent = nome;
+      itemLista.setAttribute('role', 'listitem');
+      listaAmigos.appendChild(itemLista);
+    }
+  }
